864db69da1ca6f291e809be514d488311998ad34
VP <51seer.vincent@gmail.com>
2024-02-20 14:03:54 +0800

    Update expenv.sh

--- eBPF/redirect_icmp/docker/expenv.sh
+++ eBPF/redirect_icmp/docker/expenv.sh
@@ -18,10 +18,11 @@ do
   ipincontainer=$(sudo nsenter -t "$containerpid" -n ip -4 addr show eth0 | grep inet | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
   vethname=$(ip a | grep "${ifnum}: " | cut -d '@' -f 1 | cut -d ' ' -f 2)
   echo "ID: ${containerid} PID: ${containerpid} ifnum: ${ifnum} vethname: ${vethname} IPinContainer: ${ipincontainer} NAME: ${containername}"
-  # Enable NAPI and disable checksum offload/verify
+  # Enable NAPI and disable checksum offload/verify and rp_filter
   sudo ethtool -K "${vethname}" gro on
   sudo ethtool -K "${vethname}" tx off
   sudo ethtool -K "${vethname}" rx off
+  echo 0 | sudo tee /proc/sys/net/ipv4/conf/${vethname}/rp_filter
   case "$containername" in
     "client")
         clientnic=$vethname
