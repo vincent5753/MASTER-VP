803acb20ba417a322053d1f0aeb8751d32d5ab87
VP <51seer.vincent@gmail.com>
2024-02-20 10:27:25 +0800

    Update getdockerveth.sh

--- eBPF/getdockerveth.sh
+++ eBPF/getdockerveth.sh
@@ -8,6 +8,7 @@ do
   containername=$(echo ${line} | awk -F " " '{print $2}')
   containerpid=$(docker inspect --format '{{.State.Pid}}' "${containerid}")
   ifnum=$(sudo nsenter -t "$containerpid" -n ip addr | grep "eth0@" | cut -d '@' -f 2 | cut -d ':' -f 1 | sed 's/if//g')
+  ipincontainer=$(sudo nsenter -t "$containerpid" -n ip -4 addr show eth0 | grep inet | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
   vethname=$(ip a | grep "${ifnum}: " | cut -d '@' -f 1 | cut -d ' ' -f 2)
-  echo "ID: ${containerid} PID: ${containerpid} ifnum: ${ifnum} vethname: ${vethname} NAME: ${containername}"
+  echo "ID: ${containerid} PID: ${containerpid} ifnum: ${ifnum} vethname: ${vethname} IPinContainer: ${ipincontainer} NAME: ${containername}"
 done < <(echo "$containers")
