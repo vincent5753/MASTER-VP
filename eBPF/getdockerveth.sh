c7bcd560a35a5eeeb56b57e75292d007d807b1ea
VP <51seer.vincent@gmail.com>
2024-02-22 11:43:26 +0800

    Update getdockerveth.sh

--- eBPF/getdockerveth.sh
+++ eBPF/getdockerveth.sh
@@ -11,4 +11,20 @@ do
   ipincontainer=$(sudo nsenter -t "$containerpid" -n ip -4 addr show eth0 | grep inet | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
   vethname=$(ip a | grep "${ifnum}: " | cut -d '@' -f 1 | cut -d ' ' -f 2)
   echo "ID: ${containerid} PID: ${containerpid} ifnum: ${ifnum} vethname: ${vethname} IPinContainer: ${ipincontainer} NAME: ${containername}"
+  nicebpf=$(ip a | grep ${vethname})
+  xdpdrvresult=$(echo ${nicebpf} | grep "xdp/")
+  xdpgenericresult=$(echo ${nicebpf} | grep "xdpgeneric/")
+  [[ ! -z ${xdpdrvresult} ]] && nicebpfmode="xdpdrv"
+  [[ ! -z ${xdpgenericresult} ]] && nicebpfmode="xdpgeneric"
+  case ${nicebpfmode} in
+    xdpdrv)
+        echo "${vethname} mounted eBPF in xdpdrv mode!"
+        ;;
+    xdpgeneric)
+        echo "${vethname} mounted eBPF in xdpgeneric mode!"
+        ;;
+    *)
+        echo "${vethname} has no eBPF mounted!"
+        ;;
+  esac
 done < <(echo "$containers")
