c9f94c3ccfdd93ca840bf5c0e0291145edce50bf
VP <51seer.vincent@gmail.com>
2024-03-22 15:14:52 +0800

    Bug fix for exited containers.

--- eBPF/getdockerveth.sh
+++ eBPF/getdockerveth.sh
@@ -12,6 +12,10 @@ do
   containerid=$(echo ${line} | awk -F " " '{print $1}')
   containername=$(echo ${line} | awk -F " " '{print $2}')
   containerpid=$(docker inspect --format '{{.State.Pid}}' "${containerid}")
+    if [ "${containerpid}" -eq 0 ]
+    then
+      break
+    fi
   ifnum=$(sudo nsenter -t "$containerpid" -n ip addr | grep "eth0@" | cut -d '@' -f 2 | cut -d ':' -f 1 | sed 's/if//g')
   ipincontainer=$(sudo nsenter -t "$containerpid" -n ip -4 addr show eth0 | grep inet | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
   vethname=$(ip a | grep "${ifnum}: " | cut -d '@' -f 1 | cut -d ' ' -f 2)
@@ -87,7 +91,7 @@ do
       done <<< "${eebpfresult}"
     else
       echo "[eBPF][tc] To ${grn}attach${end} eBPF onto tc, use following command:"
-      echo "[eBPF][tc]    sudo tc filter add dev ${vethname} egress bpf da obj \${objname}.o sec \${secname}"
+      echo "[eBPF][tc]    ${yel}sudo tc filter add dev ${vethname} egress bpf da obj${end} \${objname}.o sec \${secname}"
     fi
     echo "[eBPF][tc]"
 
