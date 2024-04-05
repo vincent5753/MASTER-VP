da1ddb64418161dfba3ff26d1b5bf46bfa4e3bd7
VP <51seer.vincent@gmail.com>
2024-04-05 19:29:29 +0800

    Create pre-env.sh

---
+++ free5gcenv/pre-env.sh
@@ -0,0 +1,23 @@
+# k8s 1.24
+curl https://raw.githubusercontent.com/vincent5753/KAIS/main/legacy/Ubuntu2004-K8s_1_24-containerd-flannel.sh | bash
+
+# eBPF-related
+sudo apt install -y clang llvm
+sudo apt install -y libbpf-dev libelf-dev libpcap-dev gcc-multilib build-essential
+sudo apt install -y linux-headers-$(uname -r) linux-tools-common linux-tools-generic linux-tools-$(uname -r)
+sudo apt install -y clang libmnl-dev bison flex pkg-config
+
+# upgrade and compile iproute2 to support BTF
+wget https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-6.8.0.tar.xz
+tar xvJf iproute2-6.8.0.tar.xz
+cd iproute2-6.8.0
+./configure --libbpf_force on --prefix=/usr --color always
+make
+sudo make install
+
+# gtp5g
+git clone https://github.com/free5gc/gtp5g.git
+sudo apt install -y gcc make
+cd gtp5g
+make clean && sudo make
+sudo make install
